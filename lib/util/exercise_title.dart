c151f898a0148b16ee35368e839a7597f577fd4c
Kimi Maulana <kimi93.com@gmail.com>
2025-03-19 00:04:41 +0700

    add new file

---
+++ lib/util/exercise_title.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class ExerciseTitle extends StatefulWidget {
+  const ExerciseTitle({Key? key}) : super(key: key);
+
+  @override
+  State<ExerciseTitle> createState() => _ExerciseTitleState();
+}
+
+class _ExerciseTitleState extends State<ExerciseTitle> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Container(
+                  padding: EdgeInsets.all(16),
+                  color: Colors.orange,
+                  child: Icon(Icons.favorite, color: Colors.white),
+                ),
+              ),
+              SizedBox(width: 12),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //title
+                  Text(
+                    'Speaking Skills',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                  ),
+                  SizedBox(height: 5),
+                  //subtitle
+                  Text(
+                    '16 Exercises',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Icon(Icons.more_horiz),
+        ],
+      ),
+    );
+  }
+}
