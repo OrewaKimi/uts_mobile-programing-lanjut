94650c9ef08c62d7f6bbfbcdd7f68d6021efe859
OrewaKimi <kimi93.com@gmail.com>
2025-04-08 21:16:59 +0700

    update routing for main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mentalhealthapp/pages/home_page.dart';
+import 'package:mentalhealthapp/pages/messages_page.dart';
+import 'package:mentalhealthapp/pages/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const HomePage(),
+        '/messages': (context) => const MessagesPage(),
+        '/profile': (context) => const ProfilePage(),
+      },
+      initialRoute: '/',
     );
   }
 }
